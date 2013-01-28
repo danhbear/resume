51ae84b5f3b912c1970e43f7adda549575ea2d31
Daniel Bear <daniel@hulu.com>
2013-01-28 07:54:24 -0800

    Add resume, markdown, github links to top.

--- resume.rb
+++ resume.rb
@@ -22,21 +22,25 @@ end
 
 # Include our configurations from config.yaml
 configure do
-  set :config, YAML.load_file('config.yaml')['user_config']
+  config = YAML.load_file('config.yaml')
+  set :user_config, config['user_config']
+  set :github_config, config['github']
 end
 
 # Render the main page.
 get '/index.html' do
-  rfile = settings.config['file']
-  name  = settings.config['name']
+  rfile = settings.user_config['file']
+  name  = settings.user_config['name']
   title = "#{name}'s Resume"
   resume = GitHub::Markup.render(rfile, File.read(rfile))
   erb :index, :locals => {
     :title => title,
     :resume => resume,
     :author => name,
-    :key => settings.config['gkey'],
-    :filename => rfile
+    :key => settings.user_config['gkey'],
+    :filename => rfile,
+    :pdf_filename => settings.user_config['pdf_file'],
+    :github_url => settings.github_config['url']
   }
 end
 
@@ -45,8 +49,16 @@ get '/' do
   redirect '/index.html'
 end
 
-# For the plain text version of our resumes
-get '/resume.txt' do
-  content_type 'text/plain', :charset => 'utf-8'
-  File.read(settings.config['file'])
+# For the plain text markdown version of our resumes
+[ '/resume.md', '/resume.txt' ].each {|file_path|
+  get file_path do
+    content_type 'text/plain', :charset => 'utf-8'
+    File.read(settings.user_config['file'])
+  end
+}
+
+# For the pdf version of our resumes
+get '/resume.pdf' do
+  content_type 'application/pdf', :charset => 'utf-8'
+  File.read(settings.user_config['pdf_file'])
 end
